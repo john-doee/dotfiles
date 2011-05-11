321533b417dbcb881530bc0d9b9c8505c79c9111
Tomasz Tokarski <tomasz.tokarski@rapidsoftware.pl>
2011-05-11 11:51:15 +0200

    dbext

--- install.sh
+++ install.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 echo 'kopiowanie'
-cp -R .vim ~/.vim
+cp -R .vim/ ~/.vim/
 cp .vimrc ~/.vimrc
+cp .vim/autoload/dbext_dbi.vim ~/.vim/autoload/dbext_dbi.vim
+cp .vim/autoload/dbext.vim ~/.vim/autoload/dbext.vim
+cp .vim/plugin/dbext.vim ~/.vim/plugin/dbext.vim
+cp .vim/doc/dbext.txt ~/.vim/doc/dbext.txt
+cp .vim/doc/dbext_gpl.dat ~/.vim/doc/dbext_gpl.dat
