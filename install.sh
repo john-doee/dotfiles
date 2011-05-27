bc0ec3dd1f9693e057dca7f9c853ecc521c4be7f
Tomasz Tokarski <tomasz@tomasztokarski.com>
2011-05-27 18:18:42 +0200

    modified:   install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 echo 'kopiowanie'
+mkdir ~/.vim/
+mkdir ~/.vim/autoload
+mkdir ~/.vim/plugin
 cp -R .vim/ ~/.vim/
+cp -R .vim/after ~/.vim/after
+cp .vim/plugin/Tabular.vim ~/.vim/plugin/Tabular.vim
+cp .vim/autoload/tabular.vim ~/.vim/autoload/tabular.vim
 cp .vimrc ~/.vimrc
 cp .vim/autoload/dbext_dbi.vim ~/.vim/autoload/dbext_dbi.vim
 cp .vim/autoload/dbext.vim ~/.vim/autoload/dbext.vim
