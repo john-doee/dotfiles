43feab31ae29f1c8bddbe00cdc1a36aae9848de7
Tomasz Tokarski <tomasz@tomasztokarski.com>
2012-08-30 09:28:24 +0200

    pull

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo 'installation started'
 git submodule update --init
-git submodule foreach git l origin master
+git submodule foreach git pull origin master
 mkdir ~/.vim
 cp -R .vim ~
 cp .vimrc ~
